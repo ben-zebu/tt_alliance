df2c651be509d6b39514cb6a3cca0fe23f755608
Benoit SERRE <archeparcel@gmail.com>
2024-12-20 14:44:42 +0100

    Add CoefficientCommand class

--- components/abase/src/input/CommandsSpecial.h
+++ components/abase/src/input/CommandsSpecial.h
@@ -12,7 +12,7 @@ namespace abase {
             std::string _value;
 
         public :
-            SingleCommand() = default;
+            SingleCommand(const std::string& type) : CompositeCommand(type) {};
             virtual ~SingleCommand() = default;
 
             /// @brief clear read data associated to the command
@@ -21,7 +21,7 @@ namespace abase {
             /// @brief Read input file and set the value of the command
             /// @param reader file reader associated to the input file
             /// @return a status code (0 for success and 1 for fail)
-            virtual std::size_t read_input(FileReader& reade, const CommandsCollector& collector) override;
+            virtual std::size_t read_input(FileReader& reader, const CommandsCollector& collector) override;
             /// @brief Get the value of the command
             /// @param value value associated to the command
             void get_value(std::string& value) const override { if (_read_status) value = _value; }
@@ -49,7 +49,7 @@ namespace abase {
             std::vector<std::size_t> _values;
 
         public:
-            TimeStepCommand() = default;
+            TimeStepCommand(const std::string& type) : CompositeCommand(type) {};
             virtual ~TimeStepCommand() = default;
 
             /// @brief clear read data associated to the command
@@ -83,7 +83,7 @@ namespace abase {
             std::vector<double> _values;
 
         public:
-            TableCommand() = default;
+            TableCommand(const std::string& type) : CompositeCommand(type) {};
             virtual ~TableCommand() = default;
 
             /// @brief clear read data associated to the command
@@ -102,6 +102,44 @@ namespace abase {
             
     };
 
+    /// @class CoefficientCommand
+    /// @brief Class used to read a coefficient from the input file
+    class CoefficientCommand : public CompositeCommand {
+        private:
+            /// @brief Convert coefficient values from a sequence of strings
+            /// @param values sequence of strings
+            /// @param status status of the conversion
+            void convert(const std::vector<std::string>& values, bool& status);
+            /// @brief Extract all time steps from a sequence of strings
+            /// @param sequence extracted sequence
+            /// @param key key associated to the coefficient
+            /// @param filecontext file context for error message
+            void split_sequence(const std::string& sequence, const std::string & key, const std::string& filecontext);
+        protected :
+            /// @brief table name associated to the coefficient
+            std::string _table_name;
+            /// @brief Values associated to the coefficient (expected size is 0 or 1)
+            std::vector<double> _values;
+
+        public:
+            CoefficientCommand(const std::string& type) : CompositeCommand(type) {};
+            virtual ~CoefficientCommand() = default;
+
+            /// @brief clear read data associated to the command
+            virtual void clear() { _read_status = false; _table_name = ""; _values.clear(); children_clear(); }
+
+            /// @brief Read input file and set the value of the command
+            /// @param reader file reader associated to the input file
+            /// @return a status code (0 for success and 1 for fail)
+            virtual std::size_t read_input(FileReader& reader, const CommandsCollector& collector) override;
+            /// @brief Get the table name associated to the coefficient
+            /// @param name name of the table
+            void get_value(std::string& name) const override { if (_read_status) name = _table_name; }
+            /// @brief Get the values of the command associated to the coefficient
+            /// @param values values associated to the command
+            void get_values(std::vector<double>& values) const override { if (_read_status) values = _values; }
+    };
+
     /// @class FileCommand
     /// @brief Class used to read the name of a file from the input file 
     class FileCommand : public CompositeCommand {
@@ -110,7 +148,7 @@ namespace abase {
             std::string _value;
 
         public:
-            FileCommand() = default;
+            FileCommand(const std::string& type) : CompositeCommand(type) {};
             virtual ~FileCommand() = default;
 
             /// @brief clear read data associated to the command
@@ -124,6 +162,8 @@ namespace abase {
             /// @param name name of the table
             void get_value(std::string& name) const override { if (_read_status) name = _value; }            
     };
+
+
 
 }
 
