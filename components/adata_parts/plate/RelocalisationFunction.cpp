75364e4b5139f3878dad01373e6ee33620754d6a
Benoit SERRE <archeparcel@gmail.com>
2025-01-04 18:42:58 +0100

    Rename and update RelocalisationFunction

---
+++ components/adata_parts/plate/RelocalisationFunction.cpp
@@ -0,0 +1,96 @@
+#include <algorithm>
+
+#include "List.h"
+#include "RelocalisationFunction.h"
+
+using namespace adata::parts;
+            
+double RelocalisationFunction::get_relocalization_coef(const double& angle, const std::string& type) const {
+    auto it = _coefficients_.find(type);
+    if (it == _coefficients_.end()) {
+        error(translate("UNKNOWN_RELOCALIZATION_COEFFICIENT", type));
+    }
+    return it->second.get_yvalue(angle, "linear");
+}
+
+void RelocalisationFunction::build_relocalisation_function(const double& ph, const PlateCoefficients& coef1,
+                                                           const PlateCoefficients& coef2) {
+    if (coef1.Ph == coef2.Ph) error(translate("ERROR_SAME_PH", std::to_string(coef1.Ph)));
+    double ph_ratio = (ph - coef1.Ph) / (coef2.Ph - coef1.Ph);
+    if (ph_ratio < 0. || ph_ratio > 1.) error(translate("ERROR_PH_RATIO", std::to_string(ph_ratio)));
+
+    std::vector<double> merged_angles = amath::merge_and_sort_unique(coef1.angles, coef2.angles);
+
+    set_a_phi(merged_angles, build_interpolated_values("a_phi", coef1, coef2, merged_angles, ph_ratio));
+    set_b_phi(merged_angles, build_interpolated_values("b_phi", coef1, coef2, merged_angles, ph_ratio));
+    set_c_phi(merged_angles, build_interpolated_values("c_phi", coef1, coef2, merged_angles, ph_ratio));
+}
+
+std::vector<double> RelocalisationFunction::build_interpolated_values(const std::string& coef_type, 
+                                                                      const PlateCoefficients& coef1, 
+                                                                      const PlateCoefficients& coef2, 
+                                                                      std::vector<double>& merged_angles, 
+                                                                      double ph_ratio) const {
+    std::vector<double> interpolated_values;
+
+    // build tables using for interpolation
+    amath::Table T1, T2;
+    if (coef_type == "a_phi") {
+        T1 = amath::Table(coef1.angles, coef1.a_phi);
+        T2 = amath::Table(coef2.angles, coef2.a_phi);
+    } else if (coef_type == "b_phi") {
+        T1 = amath::Table(coef1.angles, coef1.b_phi);
+        T2 = amath::Table(coef2.angles, coef2.b_phi);
+    } else if (coef_type == "c_phi") {
+        T1 = amath::Table(coef1.angles, coef1.c_phi);
+        T2 = amath::Table(coef2.angles, coef2.c_phi);
+    } else {
+        throw std::invalid_argument("Unknown relocalisation coefficient type '" + coef_type + "' !");
+    }
+
+    // interpolate values
+    for (const auto &angle : merged_angles) {
+        double coef_T1 = T1.get_yvalue(angle, "linear");
+        double coef_T2 = T2.get_yvalue(angle, "linear");
+        interpolated_values.push_back(coef_T1 + ph_ratio * (coef_T2 - coef_T1));
+    }
+}
+
+void RelocalisationFunction::set_a_phi(std::vector<double> angles, std::vector<double> coefs) {
+    _coefficients_["a_phi"] = amath::Table(angles, coefs);
+}
+
+void RelocalisationFunction::set_b_phi(std::vector<double> angles, std::vector<double> coefs) {
+    _coefficients_["b_phi"] = amath::Table(angles, coefs);
+}
+
+void RelocalisationFunction::set_c_phi(std::vector<double> angles, std::vector<double> coefs) {
+    _coefficients_["c_phi"] = amath::Table(angles, coefs);
+}
+
+double RelocalisationFunction::get_a_phi(const double& angle) const {
+    return get_relocalization_coef(angle, "a_phi");
+}
+
+double RelocalisationFunction::get_b_phi(const double& angle) const {
+    return get_relocalization_coef(angle, "b_phi");
+}
+
+double RelocalisationFunction::get_c_phi(const double& angle) const {
+    return get_relocalization_coef(angle, "c_phi");
+}
+
+std::shared_ptr<RelocalisationFunction> RelocalisationFunction::clone() const {
+    std::shared_ptr<RelocalisationFunction> clone = std::make_shared<RelocalisationFunction>();
+    for (const auto& coef : _coefficients_) {
+        clone->_coefficients_[coef.first] = amath::Table(coef.second);
+    }
+    return clone;
+}
+
+void RelocalisationFunction::set_periodic_conditions() {
+    if (_type_.empty()) throw std::invalid_argument("Cannot set_periodic_conditions for empty type !");
+    
+
+    // TODO
+}
--- components/adata_parts/plate/RelocalisationFunction.h
+++ components/adata_parts/plate/RelocalisationFunction.h
@@ -3,10 +3,24 @@
 #include "Environment.h"
 #include "Table.h"
 
+#include "PlateCoefficients.h"
+
 namespace adata::parts {
 
     /// @brief Class to store relocalisation coefficients for perforated plates
-    class RelocalisationCoefficients {
+    class RelocalisationFunction {
+        private:
+            /// @brief Build interpolated values for a given type of relocalisation coefficient (a_phi, b_phi or c_phi)
+            /// @param coef_type relocalisation coefficient type (a_phi, b_phi or c_phi)
+            /// @param coef1 Plate coefficients for the first P/h ratio
+            /// @param coef2 Plate coefficients for the second P/h ratio
+            /// @param merged_angles Output list of angles values
+            /// @param ph_ratio \f$ P/h \f$ ratio used to interpolate values
+            /// @return interpolated relolcalisation coefficients a_phi, b_phi or c_phi
+            std::vector<double> build_interpolated_values(const std::string& coef_type, const PlateCoefficients& coef1, 
+                                                          const PlateCoefficients& coef2, 
+                                                          std::vector<double>& merged_angles, double ph_ratio) const;
+
         protected :
             /// @brief Return the relocalisation coefficient associated to a given angle
             /// @param angle given angle
@@ -22,8 +36,8 @@ namespace adata::parts {
 
         public:
 
-            RelocalisationCoefficients() = default;
-            virtual ~RelocalisationCoefficients() = default;
+            RelocalisationFunction() = default;
+            virtual ~RelocalisationFunction() = default;
 
             /// @brief Get the type of relocalisation coefficients
             std::string get_type() const { return _type_; }
@@ -33,6 +47,14 @@ namespace adata::parts {
             /// @brief Get the number of relocalisation coefficients stored in the object
             /// @return number of relocalisation coefficients
             std::size_t size() { return _coefficients_.size(); }
+
+            /// @brief Build Relocalisation function for two given plate coefficients
+            /// @param ph Expected \f$ P/h \f$ ratio
+            /// @param coef1 first plate coefficients
+            /// @param coef2 second plate coefficients
+            void build_relocalisation_function(const double &ph, const PlateCoefficients &coef1, const PlateCoefficients &coef2);
+
+            void NewFunction(const adata::parts::PlateCoefficients &coef1, const adata::parts::PlateCoefficients &coef2, std::vector<double> &merged_angles, double ph_ratio, std::vector<double> &a_phi_values);
 
             /// @brief Set values of \f$ a_{\phi} \f$ for several angles
             void set_a_phi(std::vector<double> angles, std::vector<double> coefs);
@@ -56,7 +78,7 @@ namespace adata::parts {
 
             /// @brief Clone the current object
             /// @return shared pointer to the cloned object
-            std::shared_ptr<RelocalisationCoefficients> clone() const;
+            std::shared_ptr<RelocalisationFunction> clone() const;
             /// @brief Set periodic coefficients if necesssary
             void set_periodic_conditions();
     };
