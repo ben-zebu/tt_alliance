2acead27a729de7b9fd69cb7b6807086d21417e0
Benoit SERRE <archeparcel@gmail.com>
2025-01-04 22:33:28 +0100

    Update interpolation build

--- components/adata_parts/plate/RelocalisationFunction.cpp
+++ components/adata_parts/plate/RelocalisationFunction.cpp
@@ -4,7 +4,13 @@
 #include "RelocalisationFunction.h"
 
 using namespace adata::parts;
-            
+
+RelocalisationFunction::RelocalisationFunction(const PlateCoefficients& plate_coef) {
+    _coefficients_["a_phi"] = amath::Table(plate_coef.angles, plate_coef.a_phi);
+    _coefficients_["b_phi"] = amath::Table(plate_coef.angles, plate_coef.b_phi);
+    _coefficients_["c_phi"] = amath::Table(plate_coef.angles, plate_coef.c_phi);
+}
+
 double RelocalisationFunction::get_relocalization_coef(const double& angle, const std::string& type) const {
     auto it = _coefficients_.find(type);
     if (it == _coefficients_.end()) {
@@ -21,37 +27,27 @@ void RelocalisationFunction::build_relocalisation_function(const double& ph, con
 
     std::vector<double> merged_angles = amath::merge_and_sort_unique(coef1.angles, coef2.angles);
 
-    set_a_phi(merged_angles, build_interpolated_values("a_phi", coef1, coef2, merged_angles, ph_ratio));
-    set_b_phi(merged_angles, build_interpolated_values("b_phi", coef1, coef2, merged_angles, ph_ratio));
-    set_c_phi(merged_angles, build_interpolated_values("c_phi", coef1, coef2, merged_angles, ph_ratio));
+    RelocalisationFunction reloc_f1(coef1);
+    RelocalisationFunction reloc_f2(coef2);
+    reloc_f1.set_periodic_conditions();
+    reloc_f2.set_periodic_conditions();
+
+    set_a_phi(merged_angles, build_interpolated_values("a_phi", reloc_f1, reloc_f2, merged_angles, ph_ratio));
+    set_b_phi(merged_angles, build_interpolated_values("b_phi", reloc_f1, reloc_f2, merged_angles, ph_ratio));
+    set_c_phi(merged_angles, build_interpolated_values("c_phi", reloc_f1, reloc_f2, merged_angles, ph_ratio));
 }
 
 std::vector<double> RelocalisationFunction::build_interpolated_values(const std::string& coef_type, 
-                                                                      const PlateCoefficients& coef1, 
-                                                                      const PlateCoefficients& coef2, 
+                                                                      const RelocalisationFunction& reloc_f1, 
+                                                                      const RelocalisationFunction& reloc_f2, 
                                                                       std::vector<double>& merged_angles, 
                                                                       double ph_ratio) const {
     std::vector<double> interpolated_values;
 
-    // build tables using for interpolation
-    amath::Table T1, T2;
-    if (coef_type == "a_phi") {
-        T1 = amath::Table(coef1.angles, coef1.a_phi);
-        T2 = amath::Table(coef2.angles, coef2.a_phi);
-    } else if (coef_type == "b_phi") {
-        T1 = amath::Table(coef1.angles, coef1.b_phi);
-        T2 = amath::Table(coef2.angles, coef2.b_phi);
-    } else if (coef_type == "c_phi") {
-        T1 = amath::Table(coef1.angles, coef1.c_phi);
-        T2 = amath::Table(coef2.angles, coef2.c_phi);
-    } else {
-        throw std::invalid_argument("Unknown relocalisation coefficient type '" + coef_type + "' !");
-    }
-
     // interpolate values
     for (const auto &angle : merged_angles) {
-        double coef_T1 = T1.get_yvalue(angle, "linear");
-        double coef_T2 = T2.get_yvalue(angle, "linear");
+        double coef_T1 = reloc_f1.get_coef(coef_type, angle);
+        double coef_T2 = reloc_f2.get_coef(coef_type, angle);
         interpolated_values.push_back(coef_T1 + ph_ratio * (coef_T2 - coef_T1));
     }
     return interpolated_values;
@@ -81,6 +77,11 @@ double RelocalisationFunction::get_c_phi(const double& angle) const {
     return get_relocalization_coef(angle, "c_phi");
 }
 
+double RelocalisationFunction::get_coef(const std::string& type, const double& angle) const {
+    return get_relocalization_coef(angle, type);
+}
+
+
 std::shared_ptr<RelocalisationFunction> RelocalisationFunction::clone() const {
     std::shared_ptr<RelocalisationFunction> clone = std::make_shared<RelocalisationFunction>();
     for (const auto& coef : _coefficients_) {
@@ -89,9 +90,55 @@ std::shared_ptr<RelocalisationFunction> RelocalisationFunction::clone() const {
     return clone;
 }
 
+void RelocalisationFunction::set_periodic_conditions_up() {  
+    if (_coefficients_.begin()->second.get_xmax() > 90.) return;
+
+    // a_phi and b_phi are symmetric between [0, 90] and [90, 180]
+    // c_phi is anti-symmetric between between [0, 90] and [90, 180]
+    std::vector<double> angles = get_angles();
+    std::reverse(angles.begin(), angles.end());
+
+    std::vector<double> sym_angles;
+    std::vector<double> sym_a_phi, sym_b_phi, sym_c_phi;
+    for (const auto& angle : angles) {
+        sym_angles.push_back(180. - angle);
+        sym_a_phi.push_back(_coefficients_["a_phi"].get_yvalue(angle, "linear"));
+        sym_b_phi.push_back(_coefficients_["b_phi"].get_yvalue(angle, "linear"));
+        sym_c_phi.push_back(-1.*_coefficients_["c_phi"].get_yvalue(angle, "linear"));
+    }
+    _coefficients_["a_phi"].expand(sym_angles, sym_a_phi);
+    _coefficients_["b_phi"].expand(sym_angles, sym_b_phi);
+    _coefficients_["c_phi"].expand(sym_angles, sym_c_phi);
+
+}
+
+void RelocalisationFunction::set_periodic_conditions_down() {  
+    if (_coefficients_.begin()->second.get_xmax() > 180.) return;
+
+    // a_phi, b_phi and c_phi are symmetric between [0, 180] and [180, 360]
+    std::vector<double> angles = get_angles();
+    std::reverse(angles.begin(), angles.end());
+
+    std::vector<double> sym_angles;
+    std::vector<double> sym_a_phi, sym_b_phi, sym_c_phi;
+    for (const auto& angle : angles) {
+        sym_angles.push_back(360. - angle);
+        sym_a_phi.push_back(_coefficients_["a_phi"].get_yvalue(angle, "linear"));
+        sym_b_phi.push_back(_coefficients_["b_phi"].get_yvalue(angle, "linear"));
+        sym_c_phi.push_back(_coefficients_["c_phi"].get_yvalue(angle, "linear"));
+    }
+    _coefficients_["a_phi"].expand(sym_angles, sym_a_phi);
+    _coefficients_["b_phi"].expand(sym_angles, sym_b_phi);
+    _coefficients_["c_phi"].expand(sym_angles, sym_c_phi);
+}
+
+
 void RelocalisationFunction::set_periodic_conditions() {
     if (_type_.empty()) throw std::invalid_argument("Cannot set_periodic_conditions for empty type !");
     
+    // Complete coefficients for angles between [90, 180] based on [0, 90]
+    set_periodic_conditions_up();
 
-    // TODO
+    // Complete coefficients for angles between [180, 360] based on [0, 180]
+    set_periodic_conditions_down();
 }
